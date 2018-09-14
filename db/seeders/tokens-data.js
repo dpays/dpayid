7ee62aa53917e3030328f7d74dcde8b6de61f56a
Jared Rice Sr <jaredricelegal@gmail.com>
2018-09-14 11:31:35 -0500

    Pushed to production

--- db/seeders/tokens-data.js
+++ db/seeders/tokens-data.js
@@ -4,7 +4,7 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('tokens', [
       {
-        client_id: 'busy.app',
+        client_id: 'dsocial.app',
         user: 'guest123',
         token: 'secret-token-123',
         created_at: new Date(),
